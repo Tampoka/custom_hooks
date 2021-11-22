29d0f69d28d656b1325cabe27cbc643486399370
Tampoka <goal.thanks@gmail.com>
2021-11-22 17:22:12 +0900

    Add debouncedSearch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from "react";
-import List from "./components/List";
+import React, {useCallback, useState} from "react";
+import useDebounce from "./hooks/useDebounce";
 
 function App() {
+    const [value,setValue]=useState('')
+    const debouncedSearch=useDebounce(search,500)
+    const onChange=e=>{
+        setValue(e.target.value)
+        debouncedSearch(e.target.value)
+    }
+
+    function  search (query) {
+        fetch(`https://jsonplaceholder.typicode.com/todos?query=`+query)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json)
+            })
+    }
    /* const ref = useRef()
     const isBlackHovering = useHover(ref)*/
     return (
@@ -9,7 +23,10 @@ function App() {
             {/*<Input/>*/}
             {/*<Hover/>*/}
             {/*<div ref={ref} style={{width: 300, height: 300, background: isBlackHovering ? 'blue' : 'black'}}/>*/}
-            <List/>
+            {/*<List/>*/}
+            <input type="text"
+                   value={value}
+            onChange={onChange}/>
         </div>
     )
         ;
